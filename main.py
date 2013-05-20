cdc0c6d8fe3d84d0e3ed0435dc7fd230b2976269
Yosuke ONOUE <onoue@likr-lab.com>
2013-05-20 16:18:14 +0900

    Moved to appengine.

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import jinja2
+import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(
+            os.path.dirname(__file__) + '/templates'),
+        extensions=['jinja2.ext.autoescape'])
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
