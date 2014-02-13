0e748c8c45c52e54f950b77cd1c957959e28b352
Yutaro Kubo <y_kubo@spookies.co.jp>
2014-02-13 21:55:34 +0900

    Grunt 導入

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    ngtemplates: {
+      collaboegm: {
+        cwd: 'static',
+        dest: 'static/scripts/templates.js',
+        options: {
+          prefix: '/',
+        },
+        src: 'partials/**/*.html'
+      }
+    },
+    ts: {
+      dev: {
+        out: 'static/scripts/collaboegm.js',
+        src: ['ts/app/app.ts'],
+        options: {
+          sourceMap: false,
+          target: 'es5',
+        },
+      },
+      prod: {
+        out: 'static/scripts/collaboegm.js',
+        src: ['ts/app/app.ts'],
+        options: {
+          removeComments: true,
+          sourceMap: false,
+          target: 'es5',
+        },
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['ts/**/*.ts'],
+        tasks: ['ts:dev'],
+      },
+      templates: {
+        files: ['static/partials/**/*.html'],
+        tasks: ['ngtemplates'],
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-angular-templates');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-ts');
+
+  grunt.registerTask('default', ['watch']);
+};
