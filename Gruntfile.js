d675efceaddaebaf677d499df3ce981a35cd1adf
Yutaro Kubo <y_kubo@spookies.co.jp>
2014-03-13 15:12:36 +0900

    タスクを定義

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,36 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    manifest: {
+      generate: {
+        dest: 'static/files.appcache',
+        options: {
+          basePath: 'static',
+          cache: ['api/users'],
+          hash: true,
+          master: ['static/index.html'],
+          timestamp: true,
+        },
+        src: [
+          'bower_components/angular/angular.min.js',
+          'bower_components/angular-cookies/angular-cookies.min.js',
+          'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+          'bower_components/jquery/jquery.min.js',
+          'bower_components/bootstrap/dist/css/bootstrap.min.css',
+          'bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.*',
+          'bower_components/angular-ui-bootstrap-bower/ui-bootstrap.min.js',
+          'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.min.js',
+          'bower_components/angular-translate/angular-translate.min.js',
+          'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
+          'bower_components/d3/d3.min.js',
+          'locations/*.json',
+          'partials/*.html',
+          'scripts/*.js',
+          'styles/*.css',
+        ],
+      },
+    },
     ngtemplates: {
       collaboegm: {
         cwd: 'static',
@@ -33,7 +63,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: ['ts/**/*.ts'],
-        tasks: ['ts:dev'],
+        tasks: ['manifest', 'ts:dev'],
       },
       templates: {
         files: ['static/partials/**/*.html'],
@@ -44,6 +74,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-angular-templates');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-manifest');
   grunt.loadNpmTasks('grunt-ts');
 
   grunt.registerTask('default', ['watch']);
