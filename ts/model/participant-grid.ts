4c9787042b3bd2c718fee8c1a82bcd1bcb64e9a3
Yutaro Kubo <y_kubo@spookies.co.jp>
2014-03-14 16:02:07 +0900

    ParticipantGrid を更新

--- ts/model/participant-grid.ts
+++ ts/model/participant-grid.ts
@@ -30,37 +30,7 @@ module egrid.model {
     }
 
     update() : JQueryPromise<ParticipantGrid> {
-      var $deferred = $.Deferred();
-      var storageKey = CollectionBase.pluralize(ParticipantGrid.type);
-
-      return $.ajax({
-          url: this.url(),
-          type: 'PUT',
-          contentType: 'application/json',
-          data: JSON.stringify({
-            nodes: this.nodes,
-            links: this.links,
-          }),
-          dataFilter: data => {
-            return this;
-          },
-        })
-        .then((p: ParticipantGrid) => {
-            return $deferred.resolve(p);
-          }, (...reasons) => {
-            // ストレージにぶち込む
-            var o = {};
-            var b = {};
-
-            b[this.key] = this;
-            o[this.projectKey] = b;
-
-            window.localStorage.setItem('unsavedItems.' + storageKey, JSON.stringify(o));
-
-            return $deferred.resolve(new ParticipantGrid(o[this.projectKey][this.key]));
-          });
-
-      return $deferred.promise();
+      return egrid.storage.add<ParticipantGrid>(this, ParticipantGrid.type, this.projectKey, this.key);
     }
 
     private url() : string {
@@ -72,46 +42,6 @@ module egrid.model {
         .then((pg: ParticipantGrid) => {
             return new ParticipantGrid(pg);
           });
-    }
-
-    private static __get(projectKey : string, participantKey : string) : JQueryPromise<ParticipantGrid> {
-      var $deferred = $.Deferred();
-      var storageKey = CollectionBase.pluralize(ParticipantGrid.type);
-
-      return $.ajax({
-          url: ParticipantGrid.url(projectKey, participantKey),
-          type: 'GET',
-          dataFilter: data => {
-            var obj = JSON.parse(data);
-
-            return new ParticipantGrid(obj);
-          },
-        })
-        .then((grid : ParticipantGrid) => {
-          // 保存する
-          var o = {};
-          var b = {};
-
-          b[participantKey] = grid;
-          o[projectKey] = b;
-
-          window.localStorage.setItem(storageKey, JSON.stringify(o));
-
-          return $deferred.resolve(grid);
-        }, () => {
-          // 取得して返す
-          var o = JSON.parse(window.localStorage.getItem(storageKey)) || {};
-          var u = JSON.parse(window.localStorage.getItem('unsavedItems.' + storageKey)) || {};
-
-          o = o[projectKey] || {};
-          u = u[projectKey] || {};
-
-          var r = $.extend(o[participantKey], u[participantKey]);
-
-          return $deferred.resolve(new ParticipantGrid(r));
-        });
-
-      return $deferred.promise();
     }
 
     private static url(projectKey : string, participantKey : string) : string {
